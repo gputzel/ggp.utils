0ea9207dc9fdd84f746a5833e354e71c5f40dfa8
Gregory Putzel <grp2009@med.cornell.edu>
2018-06-29 12:18:15 -0400

    Add saveLoad decorator as well an operator for applying decorators

---
+++ R/saveLoad.R
@@ -0,0 +1,31 @@
+#' A decorator to allow easy caching of function output
+#'
+#' @param f Function to be decorated
+#' @return Decorated function that takes an additional argument `filename`
+#' @export
+saveLoad <- function(f){
+    function(filename,...){
+        if(file.exists(filename)){
+            ## Get the answer, it's already there
+            return(readRDS(filename))
+        } else {
+            ## Run f, save the results, return value
+            res <- f(...)
+            if(!dir.exists(dirname(filename))){
+                dir.create(dirname(filename),recursive = T)
+            }
+            saveRDS(object=res,file=filename)
+            return(res)
+        }
+    }
+}
+
+#' Decorator operator
+#'
+#' @param decorator The decorator functional
+#' @param f The function to be decorated
+#' @return The decorated function
+#' @export
+`%decorate%` <- function(decorator, f) {
+    decorator(f)
+}
