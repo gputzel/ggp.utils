2beba9c954880e7fc073c98f3d9ae95fb2b7cb37
Gregory Putzel <gregputzel@gmail.com>
2021-05-24 10:22:26 -0400

    Make style changes according to lintr output

--- R/saveLoad.R
+++ R/saveLoad.R
@@ -3,18 +3,18 @@
 #' @param f Function to be decorated
 #' @return Decorated function that takes an additional argument `filename`
 #' @export
-saveLoad <- function(f){
-    function(filename,...){
-        if(file.exists(filename)){
+saveLoad <- function(f) {
+    function(filename, ...) {
+        if (file.exists(filename)) {
             ## Get the answer, it's already there
             return(readRDS(filename))
         } else {
             ## Run f, save the results, return value
             res <- f(...)
-            if(!dir.exists(dirname(filename))){
-                dir.create(dirname(filename),recursive = T)
+            if (!dir.exists(dirname(filename))) {
+                dir.create(dirname(filename), recursive = T)
             }
-            saveRDS(object=res,file=filename)
+            saveRDS(object = res, file = filename)
             return(res)
         }
     }
--- R/saveRDS.ggp.R
+++ R/saveRDS.ggp.R
@@ -2,11 +2,11 @@
 #'
 #' @param object The R value to be saved in the RDS file
 #' @export
-saveRDS.ggp <- function(object,file,...){
-    attr(object,'sessionInfo') <- sessionInfo()
-    if(rstudioapi::isAvailable()){
+saveRDS.ggp <- function(object, file, ...) {
+    attr(object, "sessionInfo") <- sessionInfo()
+    if (rstudioapi::isAvailable()) {
         #If we are in an RStudio notebook, then we have additional context to
-        attr(object,'context') <- rstudioapi::getActiveDocumentContext()
+        attr(object, "context") <- rstudioapi::getActiveDocumentContext()
     }
-    saveRDS(object = object,file=file,...)
+    saveRDS(object = object, file = file, ...)
 }
