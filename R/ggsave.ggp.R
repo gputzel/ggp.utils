9f464ed982ec880cf05e94d31cdbe8425492d962
Gregory Putzel <gregputzel@gmail.com>
2021-05-24 10:07:36 -0400

    Make style changes according to lintr output

--- R/ggsave.ggp.R
+++ R/ggsave.ggp.R
@@ -2,13 +2,19 @@
 #'
 #' @param object The R value to be saved in the RDS file
 #' @export
-ggsave.ggp <- function(filename,plot=NULL,overwrite=FALSE,...){
-    if(file.exists(filename)){
-        if(!overwrite){
-            cat(paste0('File already exists: `',gsub('[.][.]/','',filename),'`'))
+ggsave.ggp <- function(filename, plot=NULL, overwrite=FALSE, ...) {
+    if (file.exists(filename)) {
+        if (!overwrite) {
+            cat(
+                paste0(
+                    "File already exists: `",
+                    gsub("[.][.]/", "", filename),
+                    "`"
+                )
+            )
             return(FALSE)
         }
     }
-    ggplot2::ggsave(filename=filename,plot=plot,...)
-    cat(paste0('Figure saved as `',gsub('[.][.]/','',filename),'`'))
+    ggplot2::ggsave(filename = filename, plot = plot, ...)
+    cat(paste0("Figure saved as `", gsub("[.][.]/", "", filename), "`"))
 }
